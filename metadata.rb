bc9bc67a0c9800f159dd3daef86d3fea0a092e84
Jonathon W. Marshall <jonathon@bnotions.com>
2014-08-18 14:47:04 -0400

    cookbook version 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jonathon@bnotions.com'
 license          'All rights reserved'
 description      'Installs/Configures application_wordpress'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends          'build-essential'
 depends          'application', '= 3.0'
